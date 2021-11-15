46eaa167d87a49340a5b618efc84ef1e45bc3f78
nabila benzait <benzait.nabila@gmail.com>
2021-11-15 14:48:37 +0100

    rails-stupid-coaching

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,21 @@
+class QuestionsController < ApplicationController
+
+  def ask
+
+  end
+
+  def answer
+    @questions = params[:questions]
+    @answer = coach(@questions)
+  end
+
+  def coach(questions)
+    if questions == "I am going to work right now!"
+      'great'
+    elsif questions.end_with?("?")
+      "Silly question, get dressed and go to work!"
+    else
+      "I don't care, get dressed and go to work!"
+    end
+  end
+end
